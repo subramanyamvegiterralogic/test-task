35d44bae3e99cbe4762bced1ea9a50c6c7210842
subramanyamvegiterralogic <subramanyam.vegi@terralogic.com>
2019-09-05 12:29:28 +0530

    Method Created for calling

--- json_task/test_json.py
+++ json_task/test_json.py
@@ -1,25 +1,22 @@
 import re, json
 import employee as emp
 
-emp_list=[]
-with open('text_json.json','r') as file:
-    file_data = json.load(file)
-    print(file_data)
-    for key,value in file_data.items():
-        if 'data_list' in file_data.keys():
-            data_list = file_data.get(key)
-            for nested_item in range(0,len(data_list)):
-                e = emp.Employee()
-                esal = e.set_esal(data_list[nested_item].get('e_sal'))
-                eno = e.set_no(data_list[nested_item].get('e_no'))
-                ecity = e.set_city(data_list[nested_item].get('e_city'))
-                emobile = e.set_mobile(data_list[nested_item].get('e_mobile'))
-                ename = e.set_name(data_list[nested_item].get('e_name'))
-                emp_list.append(e)
-    for data in emp_list:
-        print(data.get_no())
-        print(data.get_name())
-        print(data.get_esal())
-        print(data.get_city())
-        print(data.get_mobile())
-        print('')
+def parse_json_file_data():
+    emp_list=[]
+    with open('text_json.json','r') as file:
+        file_data = json.load(file)
+        for key,value in file_data.items():
+            if 'data_list' in file_data.keys():
+                data_list = file_data.get(key)
+                for nested_item in range(0,len(data_list)):
+                    e = emp.Employee()
+                    e.set_esal(data_list[nested_item].get('e_sal'))
+                    e.set_no(data_list[nested_item].get('e_no'))
+                    e.set_city(data_list[nested_item].get('e_city'))
+                    e.set_mobile(data_list[nested_item].get('e_mobile'))
+                    e.set_name(data_list[nested_item].get('e_name'))
+                    emp_list.append(e)
+        for data in emp_list:
+            print(data.get_no(),'\t',data.get_name(),'\t',data.get_esal(),'\t',data.get_city(),'\t',data.get_mobile())
+            print('')
+parse_json_file_data()
